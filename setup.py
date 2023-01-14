a75df0b6e01ef5fa6e56d32a8685b158ea033456
belle <belle.l24358@gmail.com>
2023-01-14 03:02:08 +0000

    Add code to setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="code",
+    version="0.0.0",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+    description='',
+    packages=find_packages(exclude=[]),
+)
