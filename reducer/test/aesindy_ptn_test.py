860a24d8e0ff9c168e4bbbcaeef64e6ac3a971f9
L24358 <belle.l24358@gmail.com>
2023-01-24 23:29:22 +0000

    Read aesindy code and attempt to pass my data to it.

---
+++ reducer/test/aesindy_ptn_test.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("../../src")
+import os
+import datetime
+import pandas as pd
+import numpy as np
+from aesindy.example_lorenz import get_lorenz_data
+from aesindy.sindy_utils import library_size
+from aesindy.training import train_network
+import tensorflow.compat.v1 as tf
+
+# generate training, validation, testing data
+noise_strength = 1e-6
+training_data = get_lorenz_data(1024, noise_strength=noise_strength)
+validation_data = get_lorenz_data(20, noise_strength=noise_strength)
+
+import pdb; pdb.set_trace()
